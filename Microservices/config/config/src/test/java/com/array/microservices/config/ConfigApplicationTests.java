2a7b04e18c690ed439ec8c0d1f82aeaeed198f58
Alan Koshy <alan.koshy@array.world>
2025-04-15 15:02:30 +0300

    Added Changes to Add new 3 MicroServices and Config Server

---
+++ Microservices/config/config/src/test/java/com/array/microservices/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.array.microservices.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Microservices/one/one/src/main/java/com/array/microservices/one/one/ConfigClientController.java
@@ -0,0 +1,18 @@
+package com.array.microservices.one.one;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigClientController {
+
+    @Value("${message}")
+    private String message;
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return message;
+    }
+
+}
